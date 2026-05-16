4be3c70d9210e5cd13405fb36041fbac550bd4a0
Irfan <muhdirfanmokhtar@gmail.com>
2026-05-16 08:18:47 +0800

    Initial Commit

---
+++ MacMenu+/MacMenu_App.swift
@@ -0,0 +1,17 @@
+//
+//  MacMenu_App.swift
+//  MacMenu+
+//
+//  Created by Irfan Mokhtar on 16/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacMenu_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
